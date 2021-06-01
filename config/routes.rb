f7b1ea21e2eb10729139ce55171081f3e4ee844e
TanyaRampal <tanyarampal09@gmail.com>
2021-06-01 18:19:14 +0530

    Used rails convention(resource)

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  get "todos", to: "todos#index"          #lists all the todos
-  get "todos/create", to: "todos#create"  #adds a todo to the list
-  get "todos/:id", to: "todos#show"       #shows 1 todo based on para id
+  # get "todos", to: "todos#index"             #lists all the todos
+  # post "todos", to: "todos#create"           #adds a todo to the list
+  # get "todos/:id", to: "todos#show"          #shows 1 todo based on para id
+
+  resources :todos
 end
