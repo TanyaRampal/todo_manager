ba7a28eedb66f20edbddf84e2e3457ea58056344
TanyaRampal <tanyarampal09@gmail.com>
2021-06-01 19:37:49 +0530

    Defined 'create' and 'index' functions for table 'users'

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,8 +2,7 @@ class UsersController < ApplicationController
   skip_before_action :verify_authenticity_token
 
   def index
-    render plain: "show all users here"
-    # render plain: Users.order(:id).map { |user| user.to_displayable_string }.join("\n")
+    render plain: "List of users:\n" + Users.order(:id).map { |user| user.to_displayable_string }.join("\n")
   end
 
   # def show
@@ -12,18 +11,18 @@ class UsersController < ApplicationController
   #   render plain: todo.to_pleasant_string
   # end
 
-  # def create
-  #   name = params[:name]
-  #   email = params[:email]
-  #   password = params[:password]
-  #   new_todo = Todo.create!(
-  #     todo_text: todo_text,
-  #     due_date: due_date,
-  #     completed: false,
-  #   )
-  #   response_text = "Hey, new todo created is: #{new_todo.to_pleasant_string} with id: #{new_todo.id}"
-  #   render plain: response_text
-  # end
+  def create
+    name = params[:name]
+    email = params[:email]
+    password = params[:password]
+    new_user = Users.create!(
+      name: name,
+      email: email,
+      password: password,
+    )
+    response_text = "New user created is: #{new_user.to_displayable_string}"
+    render plain: response_text
+  end
 
   # def update
   #   id = params[:id]
--- app/models/users.rb
+++ app/models/users.rb
@@ -1,5 +1,5 @@
 class Users < ActiveRecord::Base
   def to_displayable_string
-    "\nID:#{id}, \nName:#{name}, \nEmail:#{email} \nPassword#{password} \n"
+    "\nID:#{id}, \nName: #{name}, \nEmail: #{email} \nPassword: #{password} \n"
   end
 end
