7a6f3ec3dee7091368c248621c0bb4c335584054
TanyaRampal <tanyarampal09@gmail.com>
2021-06-01 17:40:45 +0530

    Create method in todos_controller.rb for adding new todo

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
-  get "todos", to: "todos#index"
-  get "todos/:id", to: "todos#show"
+  get "todos", to: "todos#index"          #lists all the todos
+  get "todos/create", to: "todos#create"  #adds a todo to the list
+  get "todos/:id", to: "todos#show"       #shows 1 todo based on para id
 end
