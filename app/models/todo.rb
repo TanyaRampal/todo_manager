94a921afe333efd3ab36bbd8c1ab7b9062cc4a37
TanyaRampal <tanyarampal09@gmail.com>
2021-06-12 20:05:49 +0530

    Matching HTML template with rails

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,6 +1,44 @@
 class Todo < ActiveRecord::Base
-  def to_pleasant_string
-    is_completed = completed ? "[X]" : "[ ]"
-    "#{id}. #{due_date.to_s(:long)} #{todo_text} #{is_completed} "
+  def due_today?
+    due_date == Date.today
+  end
+
+  def self.overdue
+    where("due_date < ?", Date.today)
+  end
+
+  def self.due_today
+    where("due_date = ?", Date.today)
+  end
+
+  def self.due_later
+    where("due_date > ?", Date.today)
+  end
+
+  def self.completed
+    where(completed: true)
+  end
+
+  def self.show_list
+    puts "My Todo-list\n\n"
+
+    puts "Overdue\n"
+    puts overdue.map { |todo| todo.to_displayable_string }
+    puts "\n\n"
+
+    puts "Due Today\n"
+    puts due_today.map { |todo| todo.to_displayable_string }
+    puts "\n\n"
+
+    puts "Due Later\n"
+    puts due_later.map { |todo| todo.to_displayable_string }
+    puts "\n\n"
+  end
+
+  def self.mark_as_complete(id)
+    todo = self.find(id)
+    todo.completed = true
+    todo.save
+    todo
   end
 end
