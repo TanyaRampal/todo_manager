5a579a82ce5d7a646431c0245c7615236ed5a60d
TanyaRampal <tanyarampal09@gmail.com>
2021-06-21 18:40:52 +0530

    Validate user first_name, email and password before saving

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,12 +6,19 @@ class UsersController < ApplicationController
   end
 
   def create
-    User.create!(
+    new_user = User.new(
       first_name: params[:first_name],
       last_name: params[:last_name],
       email: params[:email],
       password: params[:password],
     )
+    if new_user.save
+      redirect_to "/"
+    else
+      flash[:error] = new_user.errors.full_messages.join(", ")
+      redirect_to new_user_path
+      return
+    end
     redirect_to "/"
   end
 end
